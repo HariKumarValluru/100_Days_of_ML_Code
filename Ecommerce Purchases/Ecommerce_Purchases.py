2d641d0ed847632c6b9f2c74c93c43cb8c4d7f51
Hari Kumar Valluru <anil14353@gmail.com>
2018-10-26 12:56:32 +0530

    Ecommerce purchases data

---
+++ Ecommerce Purchases/Ecommerce_Purchases.py
@@ -0,0 +1,37 @@
+# Importing the libraries and dataset
+import pandas as pd
+
+ecom = pd.read_csv('Ecommerce Purchases.csv')
+
+# checking dataframe
+ecom.head()
+
+ecom.info()
+
+# calculating the average purchase price
+ecom['Purchase Price'].mean()
+
+# calculating the highest and lowest prices
+ecom['Purchase Price'].max()
+ecom['Purchase Price'].min()
+
+# English 'en' as their Language of choice on the website
+ecom[ecom['Language'] == 'en'].count()
+
+# Job title of "Lawyer"
+ecom[ecom['Job'] == 'Lawyer'].info()
+
+# purchase during the AM and how many people made the purchase during PM
+ecom['AM or PM'].value_counts()
+
+# 5 most common Job Titles
+ecom['Job'].value_counts().head(5)
+
+# purchase that came from Lot: "90 WT"
+ecom[ecom['Lot'] == '90 WT']['Purchase Price']
+
+# email of the person
+ecom[ecom['Credit Card'] == 4926535242672853]['Email']
+
+# people have American Express as their Credit Card Provider *and made a purchase above $95 
+ecom[(ecom['CC Provider'] == 'American Express') & (ecom['Purchase Price'] > 95)].count()
