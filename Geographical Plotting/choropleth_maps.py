fef7e1dfb2981b3dbd0aea73e1ebe1f8b58712f8
Hari Kumar Valluru <anil14353@gmail.com>
2018-10-29 13:52:21 +0530

    Ploting 2011 US Agriculture Exports by State with plotly

--- Geographical Plotting/choropleth_maps.py
+++ Geographical Plotting/choropleth_maps.py
@@ -21,3 +21,27 @@ choroMap = go.Figure(data = [data], layout=layout)
 
 plot(choroMap)
 
+# Importing pandas
+import pandas as pd
+
+# Loading dataset
+dataset = pd.read_csv("2011_US_AGRI_EXPORTS.csv")
+data = dict(type = 'choropleth',
+        colorscale = 'YlOrRd',
+        locations = dataset['code'],
+        locationmode = 'USA-states',
+        z = dataset['total exports'],
+        text = dataset['text'],
+        marker = dict(
+                line = {
+                        'color' : 'rgb(255,255,255)',
+                        'width': 2
+                        }
+                ),
+        colorbar = {'title' : 'Millions USD'}
+        )
+layout = dict(title = '2011 US Agriculture Exports by State',
+              geo={'scope':'usa','showlakes':True, 
+                   'lakecolor': 'rgb(85,173,214)'})
+choroMap2 = go.Figure(data = [data], layout=layout)
+plot(choroMap2)
