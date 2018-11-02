21d866128bc1ac4a79a312e1e5121ebf780d53c7
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-02 10:56:18 +0530

    calculating variance of x and y

--- Machine Learning/Linear Regression/linear_model/linear_regression.py
+++ Machine Learning/Linear Regression/linear_model/linear_regression.py
@@ -17,12 +17,17 @@ class LinearRegression():
         n = len(x)
         for i in range(n):
             covar += (x[i] - mean_x) * (y[i] - mean_y)
-        
         return covar
+    
+    # Calculate the variance of a list of numbers
+    def variance(self, values, mean):
+        return sum([(x - mean)**2 for x in values ])
     
     def corrcoeff(self, x, mean_x, y, mean_y):
         covar = self.covariance(x, mean_x, y, mean_y)
-        print(covar)
+        variance_x = self.variance(x, mean_x)
+        variance_y = self.variance(y, mean_y)
+        print(covar, variance_x, variance_y)
         pass
     
     def slope():
