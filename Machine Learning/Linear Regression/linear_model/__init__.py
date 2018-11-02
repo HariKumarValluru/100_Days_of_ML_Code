8a452f8fa1063b763b24011c51b3af3704a7a25d
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-02 07:49:55 +0530

    Creating LinearRegression Model

--- Machine Learning/Linear Regression/linear_model/__init__.py
+++ Machine Learning/Linear Regression/linear_model/__init__.py
@@ -1,2 +1,5 @@
 # -*- coding: utf-8 -*-
 
+from .linear_regression import LinearRegression
+
+__all__ = ['LinearRegression']
---
+++ Machine Learning/Linear Regression/linear_model/linear_regression.py
@@ -0,0 +1,10 @@
+# Linear Regression Model
+import numpy as np
+
+class LinearRegression():
+    
+    def __init__(self):
+        pass
+    
+    
+        
