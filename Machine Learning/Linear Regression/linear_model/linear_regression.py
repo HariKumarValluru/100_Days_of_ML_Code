4a27269284915a4db090d9838baa1f124ec9fd03
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-02 10:48:10 +0530

    calculating covariance

--- Machine Learning/Linear Regression/linear_model/linear_regression.py
+++ Machine Learning/Linear Regression/linear_model/linear_regression.py
@@ -6,7 +6,23 @@ class LinearRegression():
     def __init__(self):
         pass
     
-    def fit(x, y):
+    def fit(self, x, y):
+        mean_x, mean_y = np.mean(x), np.mean(y)
+        r = self.corrcoeff(x, mean_x, y, mean_y)
+        pass
+    
+    # Calculate covariance xy
+    def covariance(self, x, mean_x, y, mean_y):
+        covar = 0.0
+        n = len(x)
+        for i in range(n):
+            covar += (x[i] - mean_x) * (y[i] - mean_y)
+        
+        return covar
+    
+    def corrcoeff(self, x, mean_x, y, mean_y):
+        covar = self.covariance(x, mean_x, y, mean_y)
+        print(covar)
         pass
     
     def slope():
