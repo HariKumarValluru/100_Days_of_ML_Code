72f8494c82b4af26b897b49ba60dab5e5e009a2d
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-03 20:20:20 +0530

    fitting multiple linear regression to the training set:

--- Machine Learning/Regression/multiple_linear_regression.py
+++ Machine Learning/Regression/multiple_linear_regression.py
@@ -21,5 +21,14 @@ X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2,
+                                                    random_state = 0)
+
+# Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = model.predict(X_test)
