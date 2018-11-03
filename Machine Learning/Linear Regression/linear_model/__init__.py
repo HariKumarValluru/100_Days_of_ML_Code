143b7bce714f75b54185acb60d82eac40a06c850
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-03 18:59:37 +0530

    modified train_test_split, renamed _split.py to utills.py

--- Machine Learning/Linear Regression/linear_model/__init__.py
+++ Machine Learning/Linear Regression/linear_model/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from .linear_regression import LinearRegression
-from ._split import train_test_split
+from .utils import train_test_split
 
 __all__ = ['LinearRegression',
            'train_test_split'
--- Machine Learning/Linear Regression/simple_linear_regression.py
+++ Machine Learning/Linear Regression/simple_linear_regression.py
@@ -9,7 +9,7 @@ dataset = pd.read_csv("EcommerceCustomers.csv")
 X = dataset['Length of Membership'].values
 y = dataset['Yearly Amount Spent'].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=101)
 
 model = LinearRegression()
 model.fit(X_train, y_train)
