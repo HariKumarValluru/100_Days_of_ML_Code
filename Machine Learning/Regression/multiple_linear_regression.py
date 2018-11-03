15f3c80256a5e3c358cd982886c33e86db92ad87
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-03 19:36:50 +0530

    avoiding the dummy variable trap and splitting the dataset into the Training set and Test set

--- Machine Learning/Regression/multiple_linear_regression.py
+++ Machine Learning/Regression/multiple_linear_regression.py
@@ -16,3 +16,10 @@ labelencoder = LabelEncoder()
 X[:, 3] = labelencoder.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
+
+# Avoiding the Dummy Variable Trap
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
