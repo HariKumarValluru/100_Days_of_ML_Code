ccadf49127fb9a0c4e6c9c787d8a1a60c5c1a340
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-04 17:29:40 +0530

    implementing multiple linear regression

--- Machine Learning/Regression/multiple_linear_regression_model.py
+++ Machine Learning/Regression/multiple_linear_regression_model.py
@@ -1,1 +1,18 @@
 # Mulitple Lineear Regression without scikit learn
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('datasets/50_Startups.csv')
+
+n,_ = dataset.shape
+
+x0 = np.ones(n)
+X = dataset.iloc[:, 0:3].values
+X = np.array([x0, X[0], X[1], X[2]]).T
+
+# Initial Coefficients
+B = np.array([0, 0, 0])
+
+Y = dataset.iloc[:, -1].values
+alpha = 0.0001
