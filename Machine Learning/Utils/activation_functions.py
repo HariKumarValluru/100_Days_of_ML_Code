24d86814affe3824d63fa64785c089ad78c91d6d
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-06 23:35:09 +0530

    code cleanup

--- Machine Learning/Utils/activation_functions.py
+++ Machine Learning/Utils/activation_functions.py
@@ -5,6 +5,3 @@ class Sigmoid():
     """ Sigmoid Activation Function (1/1+e**-x)"""
     def __call__(self, x):
         return 1 / (1 + np.exp(-x))
-    
-    def gradient(self, x):
-        return self.__call__(x) * (1 - self.__call__(x))
