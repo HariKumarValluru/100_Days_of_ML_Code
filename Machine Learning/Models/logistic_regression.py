e97ee3ff7eaddc6a63aa991c83a9fd7d8963622f
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-06 18:43:08 +0530

    intialized the required params

--- Machine Learning/Models/logistic_regression.py
+++ Machine Learning/Models/logistic_regression.py
@@ -2,4 +2,19 @@
 import numpy as np
 
 class LogisticRegression:
-    pass
+    """ Logistic Regression classifier.
+    Parameters:
+    -----------
+    learning_rate: float
+    gradient_descent: boolean
+    """
+    def __init__(self, learning_rate=0.01, gradient_descent=True):
+        self.learning_rate = learning_rate
+        self.gradient_descent = gradient_descent
+        
+    def fit(self, X, y, iters =1000):
+        n_features = X.shape[1]
+        limit = 1 / np.sqrt(n_features)
+        
+        return n_features, limit
+        
