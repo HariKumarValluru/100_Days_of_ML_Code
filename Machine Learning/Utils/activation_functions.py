834450b82cbd746901ef62a42e5e6f3c6aec00e5
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-06 19:06:24 +0530

    sigmoid activation function

--- Machine Learning/Utils/activation_functions.py
+++ Machine Learning/Utils/activation_functions.py
@@ -1,4 +1,8 @@
 # Collection of activation functions
+import numpy as np
 
 class Sigmoid:
-    def __init__(self): pass
+    """ Sigmoid Activation Function (1/1+e**-x)"""
+    def __init__(self, x):
+        return 1 / (1 + np.exp(-x))
+        
