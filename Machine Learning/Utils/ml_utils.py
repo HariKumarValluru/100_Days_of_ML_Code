3ac050dfb62c062c3993b8e9cf0b1d86e9eb29e8
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-06 13:51:06 +0530

    Normalize the dataset

--- Machine Learning/Utils/ml_utils.py
+++ Machine Learning/Utils/ml_utils.py
@@ -131,3 +131,9 @@ def accuracy_score(y_true, y_pred):
     """ Compare y_true to y_pred and return the accuracy """
     accuracy = np.sum(y_true == y_pred, axis=0) / len(y_true)
     return accuracy
+
+def normalize(X, axis=-1, order=2):
+    """ Normalize the dataset X """
+    l2 = np.atleast_1d(np.linalg.norm(X, order, axis))
+    l2[l2 == 0] = 1
+    return X / np.expand_dims(l2, axis)
