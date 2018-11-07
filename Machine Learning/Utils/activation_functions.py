6aed36fbc345b4bf2d1d3c181fa50aaccfd57d76
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-07 17:11:08 +0530

    updated comments

--- Machine Learning/Utils/activation_functions.py
+++ Machine Learning/Utils/activation_functions.py
@@ -2,6 +2,7 @@
 import numpy as np
 
 class Sigmoid():
+    """ Outputs probability between 0 and 1, used to help define our logistic regression curve """
     """ Sigmoid Activation Function (1/1+e**-x)"""
     def __call__(self, x):
         return 1 / (1 + np.exp(-x))
