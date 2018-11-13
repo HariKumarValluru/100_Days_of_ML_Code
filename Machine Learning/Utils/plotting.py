4cc555ba8b4e80e285db19ba374eadb8187084f3
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-13 15:39:09 +0530

    create a mesh of plots to plot in

--- Machine Learning/Utils/plotting.py
+++ Machine Learning/Utils/plotting.py
@@ -57,3 +57,11 @@ class Plot():
         plt.ylabel('Principal Component 2')
 
         plt.show()
+        
+    def make_meshgrid(x, y, h=.02):
+        """Create a mesh of points to plot in"""
+        x_min, x_max = x.min() - 1, x.max() + 1
+        y_min, y_max = y.min() - 1, y.max() + 1
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                             np.arange(y_min, y_max, h))
+        return xx, yy
