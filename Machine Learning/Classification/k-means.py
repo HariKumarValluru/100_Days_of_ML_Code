31d1b616a72424cb6391baa470359e02b96e20d7
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-14 22:46:16 +0530

    fitted k-means and evaluted

--- Machine Learning/Classification/k-means.py
+++ Machine Learning/Classification/k-means.py
@@ -18,3 +18,20 @@ sns.scatterplot('Outstate', 'F.Undergrad', data=dataset, hue='Private')
 
 g = sns.FacetGrid(dataset, hue='Private', height=6, aspect=2)
 g = g.map(plt.hist, 'Outstate', bins=20, alpha=.6)
+
+from sklearn.cluster import KMeans
+model = KMeans(n_clusters=2)
+model.fit(dataset.drop('Private', axis=1))
+
+# Evaluting the clustering
+def converter(private):
+    if private == 'Yes':
+        return 1
+    else:
+        return 0
+    
+dataset['Cluster'] = dataset['Private'].apply(converter)
+
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(dataset['Cluster'], model.labels_))
+print(classification_report(dataset['Cluster'], model.labels_))
