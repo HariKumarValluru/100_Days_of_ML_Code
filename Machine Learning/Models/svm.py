bd2e65aeaa76cf46f80f699d3b0ca8d7b8330767
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-14 19:24:13 +0530

    implementing fit method

--- Machine Learning/Models/svm.py
+++ Machine Learning/Models/svm.py
@@ -3,12 +3,18 @@
 import numpy as np
 
 class SVM:
-    """The Support Vector Machine Model.""""
-    def __init__(self):
+    """The Support Vector Machine Model."""
+    def __init__(self, c=1, kernel):
         return self
     
-    def fit(self):
+    def fit(self, X, y):
+        n_samples, n_features = np.shape(X)
+        
+        
         pass
     
-    def predict(self):
-        pass
+    def predict(self, X):
+        # sign( w.x + b)
+        y_pred = np.sign(np.dot(self.w, np.array(X)) + self.b)
+        
+        return y_pred
