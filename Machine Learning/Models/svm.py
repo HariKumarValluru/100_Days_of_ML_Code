bde15f752e5583d7a80f1209040773c627ce389f
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-14 19:07:09 +0530

    svm model skeleton

--- Machine Learning/Models/svm.py
+++ Machine Learning/Models/svm.py
@@ -5,5 +5,10 @@ import numpy as np
 class SVM:
     """The Support Vector Machine Model.""""
     def __init__(self):
+        return self
+    
+    def fit(self):
         pass
     
+    def predict(self):
+        pass
