9d0cc87652e06e2c0a6f6e7177f80517bbfe4f79
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-15 21:51:24 +0530

    merging movie_title and movie_ratings into a dataset and creating rating dataset

--- Machine Learning/Recommender_Systems/recommender_system.py
+++ Machine Learning/Recommender_Systems/recommender_system.py
@@ -8,3 +8,18 @@ column_names = ['user_id','item_id','rating','timestamp']
 movie_ratings = pd.read_csv('Datasets/movie_ratings.csv', sep='\t', names=column_names)
 
 movie_titles = pd.read_csv('Datasets/Movie_Id_Titles.csv')
+
+# merging movie_titles and movie_ratings based on item_id into one dataset
+dataset = pd.merge(movie_titles, movie_ratings, on='item_id')
+
+# movies with the best rating
+dataset.groupby('title')['rating'].mean().sort_values(ascending=False).head()
+
+# movies with the most ratings
+dataset.groupby('title')['rating'].count().sort_values(ascending=False).head()
+
+# creating ratings dataset
+ratings = pd.DataFrame(dataset.groupby('title')['rating'].mean())
+
+# number of ratings
+ratings['num of ratings'] = dataset.groupby('title')['rating'].count()
