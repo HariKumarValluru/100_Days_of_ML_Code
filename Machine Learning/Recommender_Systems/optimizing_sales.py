4dd6d99d101e05595269798ecf69b212bc3f8493
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-18 22:55:46 +0530

    training the data and visualising the results

--- Machine Learning/Recommender_Systems/optimizing_sales.py
+++ Machine Learning/Recommender_Systems/optimizing_sales.py
@@ -11,3 +11,11 @@ dataset = pd.read_csv('Datasets/Market_Basket_Optimisation.csv', header = None)
 transactions = []
 for i in range(0, 7501):
     transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+    
+# Training Apriori on the dataset
+from Models.apyori import apriori
+rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2,
+                min_lift = 3, min_length = 2)
+
+# Visualising the results
+results = list(rules)
