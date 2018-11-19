050913270718a982e80a27a186a838961b88c9c9
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-19 22:35:16 +0530

    reading messages using pandas and creating message length column

--- Machine Learning/NLP/spam_filter.py
+++ Machine Learning/NLP/spam_filter.py
@@ -13,4 +13,19 @@ print(messages[0])
 
 for i, message in enumerate(messages[:10]):
     print(i, message + "\n")
-    
+
+import pandas as pd
+
+messages = pd.read_csv("Datasets/SMS_Spam_Collection.csv", sep="\t", 
+                       names=['label', 'message'])
+
+messages.head()
+
+messages.describe()
+
+messages.groupby('label').describe()
+
+# new column length of message
+messages['length'] = messages['message'].apply(len)
+
+messages.head()
