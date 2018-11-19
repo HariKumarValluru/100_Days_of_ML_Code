50ed8b0da3743b37a9dbe89c00fed3ada5a33372
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-19 23:00:21 +0530

    processing messages by using nltk stopwords

--- Machine Learning/NLP/spam_filter.py
+++ Machine Learning/NLP/spam_filter.py
@@ -40,3 +40,27 @@ messages['length'].describe()
 messages[messages['length'] == 910]['message'].iloc[0]
 
 messages.hist(column='length', by='label', bins=60, figsize=(12,5))
+
+import string
+
+from nltk.corpus import stopwords
+
+# checking list of stop words
+stopwords.words('english')
+
+def txt_process(message):
+    """
+    1. remove punctuation
+    2. remove stop words
+    3. return list of clean text words
+    """
+    no_punc = [char for char in message if char not in string.punctuation]
+    
+    no_punc = "".join(no_punc)
+    
+    return [word for word in no_punc.split() if word.lower not in 
+            stopwords.words('english')]
+    
+messages['message'].head(5).apply(txt_process)
+    
+    
