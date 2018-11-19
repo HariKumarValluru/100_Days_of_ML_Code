3adf50e97ccc4955e78945ca000f06c07d617004
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-19 22:28:04 +0530

    reading messages and printing the first 10

--- Machine Learning/NLP/spam_filter.py
+++ Machine Learning/NLP/spam_filter.py
@@ -1,1 +1,16 @@
 # Spam Filter
+import nltk
+
+#nltk.download()
+
+# reading messages from the file
+messages = [line.rstrip() for line in open("Datasets/SMS_Spam_Collection.csv")]
+
+print(len(messages))
+
+# checking one item
+print(messages[0])
+
+for i, message in enumerate(messages[:10]):
+    print(i, message + "\n")
+    
