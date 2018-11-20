91fc4cfc176ff127fb7559f08eabf3ff15f5e0ab
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-20 20:22:10 +0530

    creating CountVectorizer and training the model

--- Machine Learning/NLP/yelp_reviews.py
+++ Machine Learning/NLP/yelp_reviews.py
@@ -37,3 +37,35 @@ stars
 stars.corr()
 
 sns.heatmap(stars.corr(), cmap = 'coolwarm', annot=True)
+
+yelp_class = yelp[(yelp['stars'] == 1) | (yelp['stars'] == 5)]
+
+yelp.info
+
+X = yelp_class['text']
+y = yelp_class['stars']
+
+# create a CountVectorizer object
+from sklearn.feature_extraction.text import CountVectorizer
+
+cv = CountVectorizer()
+
+X = cv.fit_transform(X)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, 
+                                                    random_state=101)
+
+# training model
+from sklearn.ensemble import RandomForestClassifier
+
+rf = RandomForestClassifier()
+
+rf.fit(X_train, y_train)
+
+predictions = rf.predict(X_test)
+
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test, predictions))
+print(classification_report(y_test, predictions))
