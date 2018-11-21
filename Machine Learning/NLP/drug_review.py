86554819e84b45966d8a17ccce04ecf9b5c0ace3
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-21 19:17:36 +0530

    data processing

--- Machine Learning/NLP/drug_review.py
+++ Machine Learning/NLP/drug_review.py
@@ -12,13 +12,21 @@ train_set = pd.read_csv('Datasets/drugsComTrain.tsv', sep="\t")
 test_set = pd.read_csv('Datasets/drugsComTest.tsv', sep="\t")
 
 train_set.head()
+test_set.head()
 
 train_set.info()
+test_set.info()
 
 train_set.describe()
+test_set.describe()
 
-# adding length to training set
+# adding length, train, test on to training and test set
 train_set['text length'] = train_set['review'].apply(len)
+
+test_set['text length'] = test_set['review'].apply(len)
+
+train_set['train_test'] = 'train'
+test_set['train_test'] = 'test'
 
 g = sns.FacetGrid(train_set, 'rating')
 g.map(plt.hist, 'text length', bins=70)
@@ -34,3 +42,7 @@ ratings
 ratings.corr()
 
 sns.heatmap(ratings.corr(), cmap='coolwarm', annot=True)
+
+# merging training and test sets
+merged_set = pd.concat([train_set,test_set],axis=0)
+
