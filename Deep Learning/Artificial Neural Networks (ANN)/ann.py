78b09e04bfc40b7b2d4bb2360d2f2515c0a05caa
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-23 21:55:24 +0530

    encoding the categorical data and splitting it into training and test set

--- Deep Learning/Artificial Neural Networks (ANN)/ann.py
+++ Deep Learning/Artificial Neural Networks (ANN)/ann.py
@@ -3,5 +3,24 @@
 import numpy as np
 import pandas as pd
 
-# Data Preprocessing
+# reading the data
 dataset = pd.read_csv("Churn_Modelling.csv")
+
+# Data Preprocessing
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, -1].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, 
+                                                    random_state=0)
