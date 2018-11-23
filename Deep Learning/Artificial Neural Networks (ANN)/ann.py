6ec4f15844479b6e45767cbbb455198385465078
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-23 22:17:17 +0530

    initialising the ANN and making predictions

--- Deep Learning/Artificial Neural Networks (ANN)/ann.py
+++ Deep Learning/Artificial Neural Networks (ANN)/ann.py
@@ -31,3 +31,35 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+# Now let's make the ANN!
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(24, activation='relu', kernel_initializer='uniform', 
+                     input_dim = 11))
+
+# Adding the second hidden layer
+classifier.add(Dense(24, activation='relu', kernel_initializer='uniform'))
+
+# Adding the output layer
+classifier.add(Dense(1, activation='sigmoid', kernel_initializer='uniform'))
+
+# Compiling the ANN
+classifier.compile(optimizer='adam', loss = 'binary_crossentropy', metrics=['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train,batch_size=10,
+          epochs=50)
+
+# Making the predictions and evaluating the model
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
