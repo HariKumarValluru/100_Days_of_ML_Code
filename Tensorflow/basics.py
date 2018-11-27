ae8a6c51cec3724a1e8545166550aa25424ab2c4
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-27 22:27:08 +0530

    creating a constant and session

--- Tensorflow/basics.py
+++ Tensorflow/basics.py
@@ -1,1 +1,16 @@
 # Tensorflow basics
+
+# importing tensorflow
+import tensorflow as tf
+
+# creating a constant
+hello = tf.constant("Hello World")
+type(hello)
+
+x = tf.constant(100)
+type(x)
+
+# creating a tensorflow session
+sess = tf.Session()
+
+sess.run(hello)
