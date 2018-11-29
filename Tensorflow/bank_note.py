82f47829a866c0cafa54cebfed6e94632c7309cc
Hari Kumar Valluru <anil14353@gmail.com>
2018-11-29 21:59:10 +0530

    predicting the data

--- Tensorflow/bank_note.py
+++ Tensorflow/bank_note.py
@@ -31,3 +31,27 @@ y = dataset['Class']
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, 
                                                     random_state=101)
+
+import tensorflow as tf
+
+cols = []
+for col in features.columns:
+    cols.append(tf.feature_column.numeric_column(col))
+    
+classifier = tf.estimator.DNNClassifier(hidden_units=[10,20,10], n_classes=2,
+                                        feature_columns=cols)
+
+input_func = tf.estimator.inputs.pandas_input_fn(x=X_train, y=y_train, 
+                                                 batch_size=20, shuffle=True)
+
+classifier.train(input_func, steps=500)
+
+pred_fn = tf.estimator.inputs.pandas_input_fn(x=X_test, batch_size=len(X_test),
+                                              shuffle=False )
+note_predictions = list(classifier.predict(pred_fn))
+
+preds = []
+for pred in note_predictions:
+    preds.append(pred['class_ids'][0])
+    
+
