e51fd71b33e4810a4969e52cd078f91ae17238c0
Hari Kumar Valluru <anil14353@gmail.com>
2018-12-02 23:11:16 +0530

    creating data structure with timesteps

--- Deep Learning/Recurrent Neural Networks (RNN)/rnn.py
+++ Deep Learning/Recurrent Neural Networks (RNN)/rnn.py
@@ -10,3 +10,16 @@ import pandas as pd
 # Importing the training set
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:, 1:2].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(training_set)
+
+# Creating a data structure with 60 timesteps and 1 output
+X_train = []
+y_train = []
+for i in range(60, 1258):
+    X_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
