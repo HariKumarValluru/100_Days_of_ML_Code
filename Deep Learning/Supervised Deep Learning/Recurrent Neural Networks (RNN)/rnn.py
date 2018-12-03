05a603e3126b9ed7d3710b6b47f30b78e9fc330d
Hari Kumar Valluru <anil14353@gmail.com>
2018-12-03 12:45:47 +0530

    reshaping the data and predicting the stock price

--- Deep Learning/Supervised Deep Learning/Recurrent Neural Networks (RNN)/rnn.py
+++ Deep Learning/Supervised Deep Learning/Recurrent Neural Networks (RNN)/rnn.py
@@ -67,9 +67,21 @@ dataset_test = pd.read_csv("Datasets/Google_Stock_Price_Test.csv")
 reals_stock_price = dataset_test.iloc[:, 1:2].values
 
 # predicting the stock price of 2017
-dataset_total = pd.concat((dataset_train['open'], dataset_test['open']), axis=0)
+dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis=0)
 inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
 
 # reshaping the inputs
 inputs = inputs.reshape(-1, 1)
 inputs = sc.transform(inputs)
+
+X_test = []
+for i in range(60, 80):
+    X_test.append(inputs[i-60:i, 0])
+    
+X_test = np.array(X_test)
+
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+predicted_stock_price = regressor.predict(X_test)
+
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
