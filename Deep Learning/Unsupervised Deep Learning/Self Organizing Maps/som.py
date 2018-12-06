1c8da3fa5342a004b7ce62e42ef5938b902a319c
Hari Kumar Valluru <anil14353@gmail.com>
2018-12-06 17:41:13 +0530

    fraud detection using self organising maps

---
+++ Deep Learning/Unsupervised Deep Learning/Self Organizing Maps/som.py
@@ -0,0 +1,9 @@
+# Self Organizing Map
+
+# importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# importing the dataset
+dataset = pd.read_csv("Datasets/Credit_Card_Applications.csv")
