79e7870c20e7caf161f3a2d410311261568ad027
Hari Kumar Valluru <anil14353@gmail.com>
2018-12-07 17:57:34 +0530

    hybrid model

--- Deep Learning/Unsupervised Deep Learning/Self Organizing Maps/hybrid_model.py
+++ Deep Learning/Unsupervised Deep Learning/Self Organizing Maps/hybrid_model.py
@@ -1,2 +1,46 @@
-# -*- coding: utf-8 -*-
+# Hybrid deep learning model
 
+# importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# importing the dataset
+dataset = pd.read_csv("Datasets/Credit_Card_Applications.csv")
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# feature scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range=(0, 1))
+X = sc.fit_transform(X)
+
+# Training SOM
+from minisom import MiniSom
+som = MiniSom(x=10, y=10, input_len=15)
+som.random_weights_init(X)
+som.train_random(X, 100)
+
+# Visualising the results
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5,
+         markers[y[i]],
+         markeredgecolor = colors[y[i]],
+         markerfacecolor = 'None',
+         markersize = 10,
+         markeredgewidth = 2)
+show()
+
+# finding the frauds
+mappings = som.win_map(X)
+frauds = np.concatenate((mappings[(2,8)], mappings[(5,4)]), axis=0)
+frauds = sc.inverse_transform(frauds)
