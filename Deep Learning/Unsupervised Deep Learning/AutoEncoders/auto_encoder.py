b3f27284486e690ff052814bf2b1ac2dff061a5c
Hari Kumar Valluru <anil14353@gmail.com>
2018-12-14 20:19:28 +0530

    filled missing values and creating architecture for the autoenciders

--- Deep Learning/Unsupervised Deep Learning/AutoEncoders/auto_encoder.py
+++ Deep Learning/Unsupervised Deep Learning/AutoEncoders/auto_encoder.py
@@ -33,3 +33,25 @@ nb_movies = int(max(training_set[1].max(), test_set[1].max()))
 training_set = training_set.pivot_table(index=0, columns=1, values=2)
 test_set = test_set.pivot_table(index=0, columns=1, values=2)
 
+# fill the missing values
+training_set = training_set.fillna(0)
+test_set = test_set.fillna(0)
+
+# converting the data into Torch tensors
+training_set = torch.FloatTensor(training_set.values)
+test_set = torch.FloatTensor(test_set.values)
+
+# Creating the architecture of the autoencoders
+class SAE(nn.Module):
+    """Stacked Auto Encoder"""
+    def __init__(self, ):
+        super(SAE, self).__init__()
+        # encoding layers
+        self.fc1 = nn.Linear(nb_movies, 20)
+        self.fc2 = nn.Linear(20, 10)
+        # decoding layers
+        self.fc3 = nn.Linear(10, 20)
+        self.fc3 = nn.Linear(20, nb_movies)
+        # activation function
+        self.activation = nn.Sigmoid()
+    
