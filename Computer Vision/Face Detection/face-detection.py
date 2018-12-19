86ae8e4ac7c078b3998cd14ba9d83e84562eb774
Hari Kumar Valluru <anil14353@gmail.com>
2018-12-19 19:18:04 +0530

    implementing detect method

--- Computer Vision/Face Detection/face-detection.py
+++ Computer Vision/Face Detection/face-detection.py
@@ -4,3 +4,14 @@
 import cv2
 
 # loading the haar cascades
+face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+# eye_cascade = cv2.CascadeClassifier('cascades/haarcascade_eye.xml')
+
+def detect(frame):
+    
+    # Convert frame into gray scale image
+    gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # Detect the faces and store the positions
+    faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.3, minNeighbours=5)
+    
