2d383db98ab7e1d9c26eba192227966c476b7f02
Hari Kumar Valluru <anil14353@gmail.com>
2018-12-22 23:00:57 +0530

    App behaviour analysis

---
+++ Machine Learning/Classification/app_behavior.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from dateutil import parser
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sn
+
+dataset = pd.read_csv('appdata10.csv')
+
+dataset.head(5)
+dataset.describe()
