f6117778433a12b7bb6f9f77497ef91642bde977
Hari Kumar Valluru <anil14353@gmail.com>
2018-12-22 23:15:22 +0530

    exploratory data analysis

--- Machine Learning/Classification/app_behavior.py
+++ Machine Learning/Classification/app_behavior.py
@@ -8,3 +8,22 @@ dataset = pd.read_csv('Datasets/appdata10.csv')
 
 dataset.head(5)
 dataset.describe()
+
+# feature cleaning
+dataset["hour"] = dataset.hour.str.slice(1, 3).astype(int)
+
+# Plotting
+dataset2 = dataset.copy().drop(columns = ['user', 'screen_list', 'enrolled_date',
+                                           'first_open', 'enrolled'])
+dataset2.head()
+
+plt.suptitle('Histograms of Numerical Columns', fontsize=20)
+for i in range(1, dataset2.shape[1] + 1):
+    plt.subplot(3, 3, i)
+    f = plt.gca()
+    f.set_title(dataset2.columns.values[i - 1])
+
+    vals = np.size(dataset2.iloc[:, i - 1].unique())
+    
+    plt.hist(dataset2.iloc[:, i - 1], bins=vals, color='#3F5D7D')
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
