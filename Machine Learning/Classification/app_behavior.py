93715ef3e561bea533b892a39265bbb915a67069
Hari Kumar Valluru <anil14353@gmail.com>
2018-12-26 23:23:55 +0530

    tuning model

--- Machine Learning/Classification/app_behavior.py
+++ Machine Learning/Classification/app_behavior.py
@@ -3,6 +3,7 @@ from dateutil import parser
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sn
+import time
 
 dataset = pd.read_csv('Datasets/appdata10.csv')
 
@@ -163,3 +164,79 @@ plt.figure(figsize = (10,7))
 sn.set(font_scale=1.4)
 sn.heatmap(df_cm, annot=True, fmt='g')
 print("Test Data Accuracy: %0.4f" % accuracy_score(y_test, y_pred))
+
+
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+print("SVM Accuracy: %0.3f (+/- %0.3f)" % (accuracies.mean(), accuracies.std() * 2))
+
+# Analyzing Coefficients
+pd.concat([pd.DataFrame(dataset.drop(columns = 'user').columns, columns = ["features"]),
+           pd.DataFrame(np.transpose(classifier.coef_), columns = ["coef"])
+           ],axis = 1)
+
+
+#### Model Tuning ####
+
+## Grid Search (Round 1)
+from sklearn.model_selection import GridSearchCV
+
+# Select Regularization Method
+penalty = ['l1', 'l2']
+
+# Create regularization hyperparameter space
+C = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
+
+# Combine Parameters
+parameters = dict(C=C, penalty=penalty)
+
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = "accuracy",
+                           cv = 10,
+                           n_jobs = -1)
+t0 = time.time()
+grid_search = grid_search.fit(X_train, y_train)
+t1 = time.time()
+print("Took %0.2f seconds" % (t1 - t0))
+
+rf_best_accuracy = grid_search.best_score_
+rf_best_parameters = grid_search.best_params_
+rf_best_accuracy, rf_best_parameters
+
+
+## Grid Search (Round 2)
+
+# Select Regularization Method
+penalty = ['l1', 'l2']
+
+# Create regularization hyperparameter space
+C = [0.1, 0.5, 0.9, 1, 2, 5]
+
+# Combine Parameters
+parameters = dict(C=C, penalty=penalty)
+
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = "accuracy",
+                           cv = 10,
+                           n_jobs = -1)
+t0 = time.time()
+grid_search = grid_search.fit(X_train, y_train)
+t1 = time.time()
+print("Took %0.2f seconds" % (t1 - t0))
+
+rf_best_accuracy = grid_search.best_score_
+rf_best_parameters = grid_search.best_params_
+rf_best_accuracy, rf_best_parameters
+grid_search.best_score_
+
+
+#### End of Model ####
+
+
+# Formatting Final Results
+final_results = pd.concat([y_test, test_identity], axis = 1).dropna()
+final_results['predicted_reach'] = y_pred
+final_results = final_results[['user', 'enrolled', 'predicted_reach']].reset_index(drop=True)
