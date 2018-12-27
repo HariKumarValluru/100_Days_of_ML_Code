263b727842de906697fd088b86ec3a9d7d790c8b
Hari Kumar Valluru <anil14353@gmail.com>
2018-12-27 23:29:59 +0530

    eda

--- Machine Learning/Classification/e-signing.py
+++ Machine Learning/Classification/e-signing.py
@@ -12,3 +12,37 @@ dataset.columns
 dataset.describe()
 
 dataset.isna().any() 
+
+dataset2 = dataset.drop(columns = ['entry_id', 'pay_schedule', 'e_signed'])
+
+fig = plt.figure(figsize=(15, 12))
+plt.suptitle('Histograms of Numerical Columns', fontsize=20)
+for i in range(dataset2.shape[1]):
+    plt.subplot(6, 3, i + 1)
+    f = plt.gca()
+    f.set_title(dataset2.columns.values[i])
+
+    vals = np.size(dataset2.iloc[:, i].unique())
+    if vals >= 100:
+        vals = 100
+    
+    plt.hist(dataset2.iloc[:, i], bins=vals, color='#3F5D7D')
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+
+dataset2.corrwith(dataset.e_signed).plot.bar(
+        figsize = (20, 10), title = "Correlation with E Signed", fontsize = 15,
+        rot = 45, grid = True)
+
+sn.set(style="white")
+
+corr = dataset2.corr()
+
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+f, ax = plt.subplots(figsize=(18, 15))
+
+cmap = sn.diverging_palette(220, 10, as_cmap=True)
+
+sn.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
